f4136375916122084c34ea0d658415d8a24707bc
JonPeterP <55144487+JonPeterP@users.noreply.github.com>
2023-03-07 00:42:44 +0800

    Solve removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...theArgs) {
+
+    for (const arg of theArgs) {
+        const index = arr.indexOf(arg);
+        if(index != -1){
+            arr.splice(index, 1);
+        }
+        
+      
+    }
+  
+    console.log(arr.join(''));
+    return arr;
 
 };
 
