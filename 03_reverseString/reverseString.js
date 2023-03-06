f94750a5bc5307326417108a605ed1fd808a3317
JonPeterP <55144487+JonPeterP@users.noreply.github.com>
2023-03-07 00:22:48 +0800

    Solve reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function (str) {
+    const letters = [];
 
+    for (var i = 0; i < str.length; i++) {
+        var count = i;
+        if (i == 0) {
+            letters[i] = str.slice(-1 * (count + 1));
+        } else {
+            letters[i] = str.slice(-1 * (count + 1), -1 * (count));
+        }
+    }
+
+    return (letters.join(""));
 };
 
 // Do not edit below this line
