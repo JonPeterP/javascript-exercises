86655e9bfdf66964124ec574fe78d99bccd46cb4
JonPeterP <55144487+JonPeterP@users.noreply.github.com>
2023-03-18 17:11:22 +0800

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let n1 = 0;
+    let n2 = 1;
+    let curr = 0;
+    if(parseInt(num) < 0){
+        return "OOPS";
+    }else if(parseInt(num) == 1){
+        return 1;
+    }
+    for(let i = 1; i < parseInt(num); i++){
+        curr = n1 + n2;
+        n1 = n2;
+        n2 = curr;
+        
+       
+    }
+    return curr;
 };
 
 // Do not edit below this line
