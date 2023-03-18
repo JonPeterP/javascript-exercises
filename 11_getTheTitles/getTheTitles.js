ff57d53b371d06edeba4bf77c5fffe65b52b7a68
JonPeterP <55144487+JonPeterP@users.noreply.github.com>
2023-03-18 17:21:55 +0800

    Solve 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let inc = 0;
+    let arrTitle = [];
+    for(obj of arr){
+        arrTitle[inc] = obj.title;
+        inc++;
+    }
+    return arrTitle;
 };
 
 // Do not edit below this line
