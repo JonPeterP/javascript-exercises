8d08f9af2b9004d06aa1d2a3d65bf659814fc84c
JonPeterP <55144487+JonPeterP@users.noreply.github.com>
2023-03-18 16:45:36 +0800

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(theArgs) {
+	var sum = theArgs.reduce( (a,b) => a + b, 0);
+  
+
+  return parseInt(sum);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  var product = 1;
+  args.forEach(arg => {
+    product *= arg;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a != 0) return a * (factorial(a-1));
+  else return 1;
 };
 
 // Do not edit below this line
