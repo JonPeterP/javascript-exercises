84c7b2605c86a768a63eab944578791f39a1813f
JonPeterP <55144487+JonPeterP@users.noreply.github.com>
2023-03-18 17:39:40 +0800

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const date = new Date();
+    let persons = [];
+    let ages = [];
+    let inc = 0;
+    for(let person of people){
+        persons[inc] = person;
+        var age = person.yearOfDeath != null ? person.yearOfDeath - person.yearOfBirth : date.getFullYear() - person.yearOfBirth;
+        ages[inc] = age;
 
+        inc++;
+    }
+    let oldest = Math.max(...ages)
+    return persons[ages.indexOf(Math.max(oldest))];
 };
 
 // Do not edit below this line
