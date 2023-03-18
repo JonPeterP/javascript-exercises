bae23db8c887d6a98a94e5cf9967f81890d59217
JonPeterP <55144487+JonPeterP@users.noreply.github.com>
2023-03-18 16:55:04 +0800

    Solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ").replace(/\s/g, '').toUpperCase(); //study regex
+    console.log(reverseString(str));
+    return str == reverseString(str);
 
+};
+
+const reverseString = function (str) {
+    const letters = [];
+
+    for (var i = 0; i < str.length; i++) {
+        var count = i;
+        if (i == 0) {
+            letters[i] = str.slice(-1 * (count + 1));
+        } else {
+            letters[i] = str.slice(-1 * (count + 1), -1 * (count));
+        }
+    }
+
+    return (letters.join(""));
 };
 
 // Do not edit below this line
